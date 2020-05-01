239369f40f75d2ba1ad18e884b57e205989933f2
Dmitrii <d.kuchma98@yandex.ru>
2020-05-01 20:21:53 +0300

    initiation

---
+++ src/main/java/ru/hot/pussy/PussyApplication.java
@@ -0,0 +1,13 @@
+package ru.hot.pussy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PussyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PussyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hot/pussy/PussyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hot.pussy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PussyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
